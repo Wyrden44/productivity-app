a906cf86d87ded3e449d8375816e47cceff3e229
Pawel Gurlo <pawel.gurlo@gmail.com>
2026-02-15 18:37:11 +0100

    feat: add date to todo list and style height and margin

--- src/features/todos/components/TodoList.vue
+++ src/features/todos/components/TodoList.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import type { Todo } from '../types'
 import TodoItem from './TodoItem.vue'
+import { getDateDayMonth } from '@/utils/getters'
+import BaseHeading from '@/components/BaseHeading.vue'
 
 const props = defineProps<{
     todos: Todo[]
 }>()
+
+const date = computed(() => getDateDayMonth(new Date()))
 </script>
 
 <template>
-    <div class="todo-list flex flex-col gap-3 mt-2 w-full pl-2 pr-2 pt-4 pb-2">
+    <div class="todo-list flex flex-col gap-3 mt-2 w-full pl-2 pr-2 pt-4 pb-2 min-h-80 mb-10">
+        <BaseHeading>{{ date }}</BaseHeading>
         <TodoItem v-for="todo in props.todos" :todo="todo" :key="todo.id" />
     </div>
 </template>
