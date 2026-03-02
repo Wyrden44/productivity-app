bb0c5ed807ebda7b03736833388c2d95b6f01ffd
Pawel Gurlo <pawel.gurlo@gmail.com>
2026-03-02 17:02:05 +0100

    feat/test: add and test validators for todo list

--- frontend/src/pages/DayView.vue
+++ frontend/src/pages/DayView.vue
@@ -4,8 +4,9 @@ import BaseInput from '@/components/BaseInput.vue'
 import TimetableItem from '@/features/timetable/components/TimetableItem.vue'
 import TodoList from '@/features/todos/components/TodoList.vue'
 import { useTodoListStore } from '@/features/todos/store/todolist.store'
+import { isValidTodo } from '@/utils/validator'
 import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
+import { computed, onMounted } from 'vue'
 
 const todoStore = useTodoListStore()
 
@@ -14,12 +15,30 @@ const { todo } = storeToRefs(todoStore)
 onMounted(() => {
     todoStore.load()
 })
+
+async function onTodoSubmit() {
+    if (isValidTodo(todo.value)) {
+        await todoStore.addDraft()
+    }
+}
+
+const invalidClass = computed(() =>
+    isValidTodo(todo.value) || todo.value.length < 1
+        ? ''
+        : 'focus:decoration-wavy focus:underline focus:decoration-main-error focus:underline-offset-2 focus:border-main-error',
+)
 </script>
 
 <template>
     <section class="day-view min-w-100">
-        <form @submit.prevent="todoStore.addDraft" class="flex border-b-2 border-main-border pb-3">
-            <BaseInput type="text" v-model="todo" class="mr-2" placeholder="Todo..." />
+        <form @submit.prevent="onTodoSubmit" class="flex border-b-2 border-main-border pb-3">
+            <BaseInput
+                type="text"
+                v-model="todo"
+                class="mr-2"
+                placeholder="Todo..."
+                :class="invalidClass"
+            />
             <BaseButton size="md" type="submit">Add</BaseButton>
         </form>
         <TodoList :todos="todoStore.todos" class="mb-2" />
