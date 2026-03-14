d7116af62c1afcdbffa5898a2c78c8e10e5f3864
Pawel Gurlo <pawel.gurlo@gmail.com>
2026-03-14 17:59:55 +0100

    fix/agent fix tsconfig in backend and frontend

--- shared/eslint.config.js
+++ shared/eslint.config.js
@@ -1,7 +1,10 @@
+import { fileURLToPath } from 'node:url'
 import tseslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import prettier from 'eslint-config-prettier/flat'
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig([
     globalIgnores(['**/dist/**', '**/coverage/**']),
@@ -10,7 +13,9 @@ export default defineConfig([
         languageOptions: {
             parser: tsParser,
             parserOptions: {
+                project: './tsconfig.json',
                 sourceType: 'module',
+                tsconfigRootDir: rootDir,
             },
         },
         plugins: {
